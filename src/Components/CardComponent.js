5bd3b53d0d458596af68730a09629d9ee0689951
karthik <vs.karthik1995@gmail.com>
2021-08-07 01:05:04 +0530

    Added comment functionality.

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -1,7 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import OpenWithIcon from '@material-ui/icons/OpenWith';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import FlagIcon from '@material-ui/icons/Flag';
 import { Draggable } from "react-beautiful-dnd";
 import { Typography } from "@material-ui/core";
 import CustomProfile from '../Images/CustomProfile';
@@ -11,8 +10,7 @@ import { Modal, Button } from 'react-bootstrap';
 import Divider from '@material-ui/core/Divider';
 import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
-
-
+import TextField from '@material-ui/core/TextField';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -51,24 +49,35 @@ const CardComponent = (props) => {
     const classes = useStyles();
     const { cardItem, index } = props;
 
-
     const [expandView, setExpandView] = useState(false);
-    const [modalItem, setModalItem] = useState(null);
+    const [commentModal, setCommentModal] = useState(false);
+    const [commentAdded, setCommentAdded] = useState(null)
 
-    const handleExpand = (e, cardElement) => {
+    const handleExpand = () => {
         setExpandView(true);
-        setModalItem(cardElement);
     }
 
     const handleCloseExpand = () => {
         setExpandView(false);
-        setModalItem(null);
     }
+
+    const handleAddComment = () => {
+        setCommentModal(true);
+    }
+
+    const handleCloseComment = () => {
+        setCommentModal(false);
+    }
+
+    const onCommentInput = (e) => {
+        setCommentAdded(e.target.value)
+    }
+
     return (
         <>
             <Draggable key={cardItem.id} draggableId={cardItem.id} index={index}>
                 {(provided) => (
-                    <div onClick={(e) => handleExpand(e, cardItem)} className={classes.cardItem} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
+                    <div className={classes.cardItem} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
                         <div style={{ padding: "15px 0 0 15px" }} >
                             <Typography variant="subtitle1" style={{ fontWeight: "bold", letterSpacing: "0.5px" }}>{cardItem.title}</Typography>
                         </div>
@@ -84,9 +93,14 @@ const CardComponent = (props) => {
                                 <OpenWithIcon
                                     className={classes.cardIcon}
                                     fontSize="medium"
-                                    onClick={(e) => handleExpand(e, cardItem)}
+                                    onClick={handleExpand}
                                 />
-                                <ChatBubbleOutlineIcon className={classes.cardIcon} color="primary" fontSize="medium" />
+                                <ChatBubbleOutlineIcon
+                                    className={classes.cardIcon}
+                                    color="primary"
+                                    fontSize="medium"
+                                    onClick={(e) => handleAddComment(e, cardItem)}
+                                />
                                 <Chip style={{
                                     color: "white",
                                     fontWeight: "bold",
@@ -95,15 +109,18 @@ const CardComponent = (props) => {
                                 }}
                                     size="small"
                                     avatar={
-                                        <Avatar
+                                        <div
                                             style={{
                                                 color: customTheme.status[cardItem.status],
                                                 backgroundColor: "whitesmoke",
                                                 alignSelf: "center",
-                                                justifyContent: "center"
+                                                borderRadius: "30px"
                                             }
                                             }>
-                                            {cardItem.status[0]}</Avatar>}
+                                            <Typography style={{ fontSize: "10px", fontWeight: "bold", alignSelf: "center", margin: "2px 2px 0px 6px" }}>
+                                                {cardItem.status[0]}
+                                            </Typography>
+                                        </div>}
                                     label={cardItem.status}
                                 />
                             </div>
@@ -115,11 +132,43 @@ const CardComponent = (props) => {
                 )}
             </Draggable>
 
-            {modalItem && expandView &&
+            {commentModal &&
+                <Modal show={commentModal} onHide={handleCloseComment}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            <Typography variant="subtitle1">{`Add comment to - ${cardItem.id}`}</Typography>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="comment"
+                            label="Add comment..."
+                            type="text"
+                            fullWidth
+                            onChange={(e) => onCommentInput(e)}
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={handleCloseComment}>
+                            <Typography variant="button">Close</Typography>
+                        </Button>
+                        <Button variant="primary" onClick={() => {
+                            cardItem.comments.push(commentAdded)
+                            handleCloseComment()
+                        }}>
+                            <Typography variant="button">Save</Typography>
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            }
+
+            {cardItem && expandView &&
                 <Modal size="lg" show={expandView} onHide={handleCloseExpand}>
                     <Modal.Header closeButton>
                         <Modal.Title>
-                            <Typography variant="h6">{`${modalItem.id} - ${modalItem.title}`}</Typography>
+                            <Typography variant="subtitle1">{`${cardItem.id} - ${cardItem.title}`}</Typography>
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
@@ -128,13 +177,13 @@ const CardComponent = (props) => {
                                 <div id="description" style={{ marginBottom: "10px", height: "200px" }}>
                                     <Typography variant="subtitle1" className={classes.headingBold}>Description</Typography>
                                     <Typography variant="body2">
-                                        {modalItem.description}
+                                        {cardItem.description}
                                     </Typography>
                                 </div>
                                 <div id="comments" style={{ marginBottom: "10px" }}>
                                     <Typography variant="subtitle1" className={classes.headingBold}>Comments</Typography>
                                     {
-                                        [...modalItem.comments].reverse().map((comment, index) => {
+                                        [...cardItem.comments].reverse().map((comment, index) => {
                                             return (<Typography key={index} variant="body2" style={{ margin: "3px" }}>
                                                 {`${index + 1} - ${comment}`}
                                             </Typography>)
@@ -144,13 +193,13 @@ const CardComponent = (props) => {
                             </div>
                             <Divider style={{ border: "1px solid grey", marginRight: "30px", height: "auto" }} />
                             <div id="details" style={{ flex: 1 }} >
-                                <ModalListItem title={"ID: "} value={modalItem.id} />
-                                <ModalListItem title={"Risk: "} value={modalItem.risk} />
-                                <ModalListItem title={"Story Points: "} value={modalItem.storyPoints} />
-                                <ModalListItem title={"Status: "} value={modalItem.status} />
-                                <ModalListItem title={"Start Date: "} value={modalItem.startDate} />
-                                <ModalListItem title={"End Date: "} value={modalItem.endDate} />
-                                <ModalListItem title={"Assignee: "} value={modalItem.developer} />
+                                <ModalListItem title={"ID: "} value={cardItem.id} />
+                                <ModalListItem title={"Risk: "} value={cardItem.risk} />
+                                <ModalListItem title={"Story Points: "} value={cardItem.storyPoints} />
+                                <ModalListItem title={"Status: "} value={cardItem.status} />
+                                <ModalListItem title={"Start Date: "} value={cardItem.startDate} />
+                                <ModalListItem title={"End Date: "} value={cardItem.endDate} />
+                                <ModalListItem title={"Assignee: "} value={cardItem.developer} />
                             </div>
                         </div>
                     </Modal.Body>
